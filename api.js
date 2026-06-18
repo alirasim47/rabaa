c8e63014f681e691f570ea28b0445792fe75e555
alirasim47 <spiderat2019@gmail.com>
2026-06-18 17:03:42 +0300

    Fix database path and public folder for images

--- api.js
+++ api.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
-const { query, run } = require('../db/database');
+const { query, run } = require('./database');
 const { sendExpiryNotification } = require('./telegram');
 
 // ==========================================
